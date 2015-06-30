8c192033f3469baadf6a5569cb6aca8fe65e9459
Max P. <h.diario@gmail.com>
2015-06-30 18:41:55 -0400

    Update max_belly.rb

--- max_belly.rb
+++ max_belly.rb
@@ -4,12 +4,17 @@ max_belly_status = gets.chomp.downcase
 if max_belly_status == "no"
   puts "Ready to code!"
 elsif max_belly_status == "yes"
-  puts "Goodbye, Haagen Dazs."
+  puts "I need to eat ramen, chicken breasts, vegies, and a haagen-daz."
 else
   puts "You're not making sense. Better get some ice cream."
 end
 
 # Message to Max:
-# I saw your post and went to the grocery store for ice cream.
+# I saw your post and went to the grocery store for ice cream. 
 # It was closed. I was sad. The end.
 # 2015.06.29
+
+# Message to Celia:
+# I saw your message and laughed a lot.
+# You are so cute doing this. LOL Thanks for giving me a good laugh.
+# 2015.06.30
